042e6f4ecdf8b9c013da1ad64e7af023fc69abe7
Kyle Nathan <kyle@wearelion.com>
2015-06-12 22:27:13 +0100

    Add missing link to external server

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ module.exports = {
   contentFor: function(type, config) {
     if (type === 'head-footer') {
         var output = '<script src="/ember-cli-respond/respond.min.js"></script>';
-        if (config.respond.proxy === true) {
-            output += '<link href="/ember-cli-respond/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />' +
+        if (typeof config.respond !== 'undefined' && config.respond.proxy === true) {
+            output += '<link href="' + config.respond.externalUrl + 'respond-proxy.html" id="respond-proxy" rel="respond-proxy" />' +
+            '<link href="/ember-cli-respond/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />' +
             '<script src="/ember-cli-respond/respond.proxy.js"></script>';
         }
         return output;
