32a3de71b9c90c44898bffbdbb8e63e00b65d846
Kyle Nathan <kyle@wearelion.com>
2015-06-13 08:39:07 +0100

    Try move respond.min.js to origin

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-cli-respond',
   contentFor: function(type, config) {
     if (type === 'head-footer') {
-        var output = '<script src="' + config.respond.externalUrl + 'ember-cli-respond/respond.min.js"></script>';
+        var output = '<script src="/ember-cli-respond/respond.min.js"></script>';
         if (typeof config.respond !== 'undefined' && config.respond.proxy === true) {
             output += '<link href="' + config.respond.externalUrl + 'ember-cli-respond/respond-proxy.html" id="respond-proxy" rel="respond-proxy" />' +
             '<link href="/ember-cli-respond/respond.proxy.gif" id="respond-redirect" rel="respond-redirect" />' +
